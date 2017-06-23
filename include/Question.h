93912d8df2f1cb0131d709139014ffe179958dfa
UTopp <umberto.toppino@hotmail.com>
2017-06-23 10:36:33 +0200

    Eliminata e creata classe Question

--- include/Question.h
+++ include/Question.h
@@ -1,28 +1,14 @@
 #ifndef QUESTION_H
 #define QUESTION_H
 
-#include <string>
-#include <vector>
-#include <iterator>
-#include <algorithm>
-
-using namespace std;
 
 class Question
 {
     public:
         Question();
         virtual ~Question();
-
     protected:
-
     private:
-
-        string _question_text;
-        int _id;
-        int _number_of_questions;
-        vector<int> _possible_answers_id;
-        vector<int>::iterator it;
 };
 
 #endif // QUESTION_H
