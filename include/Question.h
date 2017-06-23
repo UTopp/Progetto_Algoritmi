329ef6ce94444f19b719895671b250556ee66c86
paoloalb <paoloalberto95@gmail.com>
2017-06-23 10:26:18 +0200

    Aggiunte variabili domanda

--- include/Question.h
+++ include/Question.h
@@ -1,6 +1,12 @@
 #ifndef QUESTION_H
 #define QUESTION_H
 
+#include <string>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
 
 class Question
 {
@@ -11,6 +17,12 @@ class Question
     protected:
 
     private:
+
+        string _question_text;
+        int _id;
+        int _number_of_questions;
+        vector<int> _possible_answers_id;
+        vector<int>::iterator it;
 };
 
 #endif // QUESTION_H
