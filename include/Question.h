56cd4ebce400b823ed3ea8275d9b4bab00b131a6
paoloalb <paoloalberto95@gmail.com>
2017-06-23 09:59:55 +0200

    Aggiunta classe per le domande

---
+++ include/Question.h
@@ -0,0 +1,16 @@
+#ifndef QUESTION_H
+#define QUESTION_H
+
+
+class Question
+{
+    public:
+        Question();
+        virtual ~Question();
+
+    protected:
+
+    private:
+};
+
+#endif // QUESTION_H
