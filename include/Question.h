8ff8830b2893934a250e5b59bac10dc373f8cba3
UTopp <umberto.toppino@hotmail.com>
2017-06-23 11:37:38 +0200

    Mod Question.h
    aggiunte variabili e metodi

--- include/Question.h
+++ include/Question.h
@@ -11,8 +11,14 @@ using namespace std;
 class Question
 {
     public:
-    Question();
-    ~Question();
+        Question();
+        ~Question();
+        void setQuestionText(string);
+        void setQuestionID(int);
+        void setNumberOfAnswers(int);
+        void setNextQuestions(int);
+        int getAnswer();
+
 
     protected:
 
@@ -20,8 +26,9 @@ class Question
 
         string _question_text;
         int _id;
-        int _number_of_questions;
-        vector<int> _possible_answers_id;
+        int _number_of_answers;
+        list<int> _possible_answers_id;
+        vector<int> _next_questions;
         vector<int>::iterator it;
 };
 
