083e191c5c5012af5f1479fddf94a330bebb1bb3
UTopp <umberto.toppino@hotmail.com>
2017-07-07 13:18:40 +0200

    Revert "Cambio branch"
    This reverts commit 44f9b86fc1582b2d44951bfe88011773571cf352.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include"coda.h"
 #include"Question.h"
 #include"AllQuestions.h"
-#include"Answer.h"
+#include"NextAnswer.h"
 
 using namespace std;
 
@@ -20,7 +20,6 @@ int main()
     */
 
     q.initializeList();
-    q.printList();
 
     return 0;
 }
